32f3d4e16094189498d6c9e7db2473c0fc48e2d6
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-02-12 20:28:19 -0300

    a

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,4 +1,4 @@
 print('Olá, Mundo!')
 nome = input ('Seu nome')
 idade = input ('Sua idade')
-print('Bem vindo',nome,'ao python no vs code',idade)
+print('Bem vindo',nome,'ao python no vs code',idade) 
