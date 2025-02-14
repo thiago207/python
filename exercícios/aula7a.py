e3211ee3d0135551a83c05edb37360af94434901
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-02-13 22:05:56 -0300

    ex e aula

---
+++ exercícios/aula7a.py
@@ -0,0 +1,18 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+rd = n1 % n2
+e = n1 ** n2
+
+
+print (f'A soma vale {s}, a mutiplicaçao vale {m}, \n a divisao vale {d},')
+
+
+
+print(f'A divisao vale {d}, \n O resto da mesma divisao vale {rd}', end='  ')
+
+
+print(f'O expoente vale {e}')
---
+++ exercícios/ex05.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite o numero: '))
+
+
+a = n1 - 1
+s = n1 + 1
+
+print(f'O seu numero é {n1}, seu sucessor é {s}, seu antecessor é {a}')
