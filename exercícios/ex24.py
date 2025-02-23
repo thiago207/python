008c704e3a644d72733a8ef389347c3824e4cd8f
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-02-23 00:00:23 -0300

    analisando os ex

--- exercícios/ex24.py
+++ exercícios/ex24.py
@@ -1,2 +1,2 @@
 cidade = input('Digite o nome de sua cidade: ').strip()
-print(f'sua cidade começa com santos? {cidade[0:5].upper() == 'SANTO'}')
+print(f'Sua cidade começa com santos? {cidade[0:5].upper() == 'SANTO'}')
