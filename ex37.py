f8e3862a126d92cd32cc23507ee5aefcb7ce9798
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-02-26 15:52:59 -0300

    ex 37 e 36

---
+++ ex37.py
@@ -0,0 +1,20 @@
+print('-'*30)
+n1 = int(input('Digite seu numero: '))
+print('-'*30)
+print('[  1  ] converter para BINÁRIO')
+print('[  2  ] converter para OCTAL')
+print('[  3  ] converter para HEXADECIMAL')
+print('-'*30)
+opcao = int(input('Sua opçao: '))
+b = bin(n1)[2:]
+o = oct(n1)[2:]
+h = hex(n1)[2:]
+if opcao == 1:
+    print(f'Seu numero {n1} em BINÁRIO fica: {b}')
+elif opcao == 2:
+    print(f'Seu numero {n1} em OCTAL fica: {o}')
+elif opcao == 3:
+    print(f'Seu numero {n1} em HEXADECIMAL fica: {h}')
+else:
+    print('Digite um numero de 1 a 3.')
+       
