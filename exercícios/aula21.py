b4c33000b3dac5d95c02fa8fe08da195aea47b81
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-04-12 14:40:06 -0300

    Create aula21.py

---
+++ exercícios/aula21.py
@@ -0,0 +1,19 @@
+'''def contador(i,f,p):
+    """
+    -> Faz uma contagem e mostra na tela.
+    :param i: inicio da contagem
+    :param f: fim da contagem
+    :param p: passo da contagem
+    :return: sem retorno
+    
+    """
+    c = i
+    while c <= f:
+        print(c)
+        c += p
+contador(0,100,10)
+help(contador)'''
+def soma (a=0,b=0,c=0):
+    s = a+b+c
+    print(s)
+soma(10,20,)
