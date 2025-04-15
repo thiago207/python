545d972ec4e1558bb1fa7954255be3a01c137fce
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-04-15 20:51:12 -0300

    ex

--- exercícios/ex107.py
+++ exercícios/ex107.py
@@ -1,13 +1,22 @@
 def ajuda(com):
     help(com)
 
+def personalizacao(linhas, cor=0):
+    print('-=-' *10)
+    print(linhas)
+    print('-=-' *10)
 
 
 
 comando = ''
 while True:
+    
     comando = str(input('FUNCAO OU BIBLIOTECA > '))
+    
     if comando.upper() =='FIM':
         break
     else:
+        personalizacao(f'   ACESSANDO O MANUAL DO {comando}')
         ajuda(comando)
+    personalizacao('    SISTEMA DE AJUDA PyHELP')
+personalizacao('   FIM DO PROGRAMA')
