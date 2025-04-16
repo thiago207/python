67f3a1c13f7c6b34694081a2fbc57d4dc9b9f671
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-04-15 22:10:00 -0300

    aula22

---
+++ exercícios/aula22/main.py
@@ -0,0 +1,13 @@
+import uteis 
+
+
+
+num = int(input('digite o valor> '))
+
+fat = uteis.fatorial(num)
+
+print(f'Seu numero {num} fatorado fica {fat}')
+
+print(f'Seu numero {num} dobrado fica {uteis.dobro(num)}')
+
+print(f'Seu numero {num} triplo fica {uteis.triplo(num)}') 
