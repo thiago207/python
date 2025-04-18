2d6e3f4b4e5e840dd931072345a712a6962e533d
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-04-17 22:29:47 -0300

    .

--- exercícios/ex111 e ex 112/utilidadescev/dado/__init__.py
+++ exercícios/ex111 e ex 112/utilidadescev/dado/__init__.py
@@ -1,9 +1,18 @@
-def leiadinheiro(msg):
+'''def leiadinheiro(msg):
     valido = False
     while not valido:
         entrada = str(input(msg)).replace(',', '.')
         if entrada.isalpha() or entrada.strip() == '':
             print(f'ERRO : {entrada} é um preço invalido! ')
+        else:
+            valido = True
+            return float(entrada)'''
+def leiadinheiro(msg):
+    valido = False
+    while not valido:
+        entrada = str(input('Digite seu preço: ')).strip().replace(',','.')
+        if entrada.isalpha() or entrada == '':
+            print(f'ERRO: {entrada} NAO É VALIDA!')
         else:
             valido = True
             return float(entrada)
