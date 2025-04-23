f0ca7b85f92e7beb4832e94066ba5d3cc45ca785
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-04-23 15:04:05 -0300

    fim curso em video.

--- exercícios/ex115/arquivo/__init__.py
+++ exercícios/ex115/arquivo/__init__.py
@@ -1,17 +1,51 @@
+import os
+from biblioteca import funcoes
 def arquivo(nome):
     try:
-        a = open(nome, 'rt')
-        a.close()
-    except:
+        with open(nome, 'rt'):
+            return True
+    except FileNotFoundError:
         return False
     else:
         return True
 
+
+
 def criar(nome):
+    pasta = os.path.dirname(nome)
+    if pasta and not os.path.exists(pasta):
+        os.makedirs(pasta)  
     try:
-        a = open(nome, 'wt+')
-        a.close()
+        with open(nome, 'wt+') as a:
+            print(f'Arquivo "{nome}" criado com sucesso!')
+    except Exception as e:
+        print(f'Houve um erro na criação do arquivo: {e}')
+
+def leraquivo(nome):
+    try:
+        a = open(nome, 'rt')
     except:
-        print('Houve um erro na criaçao do arquivo!')
+        print('Eroo ao ler o arquivo!')
     else:
-        print(f'Arquivo {nome} criado com sucesso!')
+        funcoes.linhas('PESSOAS CADASTRADAS')
+        for linha in a:
+            dados = linha.split(';')
+            dados[1] = dados[1].replace('\n','')
+            print(f'{dados[0]:<30} {dados[1]:>3} anos')
+    finally:
+        a.close()
+
+def cadastrar(arq, nome='desconhecido',idade=0):
+    try:
+        a=open(arq, 'at')
+
+    except:
+        print('Houve um erro na abertura do arquivo')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('Houve um erro na hora de escrever os dados')
+        else:
+            print(f'Novo registro de {nome} adicionado')
+            a.close()
--- exercícios/ex115/main.py
+++ exercícios/ex115/main.py
@@ -14,9 +14,12 @@ while True:
     resposta = funcoes.menu(['Ver pessoas cadrastradas' , 'Cadastrar nova pessoa' , 'Sair do progama'])
 
     if resposta == 1:
-        funcoes.linhas('op 1')
+        leraquivo(arq)
     elif resposta == 2:
-        funcoes.linhas('op 2')
+        funcoes.linhas('NOVO CADASTRO')
+        nome = str(input('NOME: '))
+        idade = funcoes.leiaint('IDADE: ')
+        cadastrar(arq, nome , idade)
     elif resposta == 3:
         funcoes.linhas('Ate logo..')
         break
