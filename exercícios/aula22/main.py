27bcb50a478cab0589ff84c6339dcff8bb8caf2a
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-04-24 22:28:40 -0300

    ajustes

--- exercícios/aula22/main.py
+++ exercícios/aula22/main.py
@@ -1,13 +1,11 @@
-import uteis 
+from uteis import numeros
 
 
 
 num = int(input('digite o valor> '))
 
-fat = uteis.fatorial(num)
+fat = numeros.fatorial(num)
 
 print(f'Seu numero {num} fatorado fica {fat}')
-
-print(f'Seu numero {num} dobrado fica {uteis.dobro(num)}')
-
-print(f'Seu numero {num} triplo fica {uteis.triplo(num)}') 
+print(f'Seu numero {num} dobrado fica {numeros.dobro(num)}')
+print(f'Seu numero {num} triplo fica {numeros.triplo(num)}')
---
+++ exercícios/aula22/uteis/numeros/__init__.py
@@ -0,0 +1,15 @@
+def fatorial(num):
+    c = 1
+    for i in range (2, num + 1):
+        c *= i
+       
+
+    return c
+
+def dobro(num):
+    return num*2
+
+def triplo(num):
+    return num*3
+
+
