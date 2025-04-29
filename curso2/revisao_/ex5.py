fc61533d7282b2ab353de6754e49d76a6c696ccc
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-04-29 20:51:22 -0300

    Update ex5.py

--- curso2/revisao_/ex5.py
+++ curso2/revisao_/ex5.py
@@ -1,7 +1,13 @@
-def soma (x=0,y=0,z=0):
-    try:
-        print(f'x + y + z = {x+y+z}')
-    except Exception as erro:
-        print(f'ERRO {erro}')
-    
-soma(2,3,8)
+def soma (x=None,y=None,z=None):
+    if x and y and z is not None:
+        try:
+            print(f'x + y + z = {x+y+z}')
+        except Exception as erro:
+            print(f'ERRO {erro}')
+    else:
+        print('DIGITE OS 3 PARAMETROS')
+
+
+soma(2,3)
+soma(6,6,8)
+soma('1',1,'u')
