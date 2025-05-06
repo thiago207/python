61972851a68f976d79d49e5289ce81b98503a13c
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-05-05 22:14:39 -0300

    Update ex23.py

--- curso2/revisao_/ex23.py
+++ curso2/revisao_/ex23.py
@@ -1,11 +1,22 @@
 # Introdução às Generator functions em Python
 # generator = (n for n in range(1000000))
-def generator(n=0, max=10):
+def generator(n=0, max=10,ord=1):
     while True:
-        yield n
-        n += 1    
-        if n > max:
-            return n
-gen = generator(0)
+        if ord == 1:
+            yield n
+            n += 1    
+            if n > max:
+                return n
+        else:
+            while True:
+                yield n
+                n += ord
+                if n > max:
+                    return n
+gen = generator(0,50,5)
 for n in gen:
     print(n)
+    
+    
+
+
