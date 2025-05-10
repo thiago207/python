c5fcbc90b897f34b50c307b3b074c2865013b250
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-05-10 20:28:31 -0300

    Update ex30.py

--- curso2/revisao_/ex30.py
+++ curso2/revisao_/ex30.py
@@ -2,5 +2,13 @@ l1 = [1,2,3,4,5,6]
 l2 = [1,2,3,4]
 
 for l1,l2 in zip(l1,l2):
-    print(l1, '+',l2,end=' = ')
-    print(f'{l1 + l2}')
+   print(l1, '+',l2,end=' = ')
+   print(f'{l1 + l2}')
+
+
+#USANDO A LOGICA SEM A FUNCAO ZIP
+#lista_soma=[]
+#for i in range(len(l2)):
+#    lista_soma.append(l1[i] + l2[i])
+#    
+#print(lista_soma)
