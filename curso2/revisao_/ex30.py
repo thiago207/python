ab0ce5bc16abbd41fa2b6a8f820e57f2e6820b98
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-05-10 21:29:13 -0300

    Update ex30.py

--- curso2/revisao_/ex30.py
+++ curso2/revisao_/ex30.py
@@ -5,7 +5,13 @@ for l1,l2 in zip(l1,l2):
    print(l1, '+',l2,end=' = ')
    print(f'{l1 + l2}')
 
+from itertools import zip_longest
+ 
+lista_a = [10, 2, 3, 4, 5]
+lista_b = [12, 2, 3, 6, 50, 60, 70]
+lista_soma = [x + y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
 
+print(lista_soma)  # [22, 4, 6, 10, 55, 60, 70]
 #USANDO A LOGICA SEM A FUNCAO ZIP
 #lista_soma=[]
 #for i in range(len(l2)):
