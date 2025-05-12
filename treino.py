7d47e0c05c4a7df2051d03be4c42acb81631af45
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-05-11 23:47:20 -0300

    Update treino.py

--- treino.py
+++ treino.py
@@ -1,28 +1,32 @@
-'''numeros = (input('DIGITE: '))
+#
+numeros = (input('DIGITE: '))
 
 l = numeros.split(',')
 print(l)
 t = tuple(l)
-print(t)'''
-
-'''from math import sqrt
+print(t)
+#
+from math import sqrt
 numeros = (input('DIGITE: '))
+
 v = numeros.split(',')
 print(v)
 lista = [ ]
 for n in v:
     v = int(n)
-    print(type(v))
+    
     q = sqrt(((2 * 50* v)/ 30))
     lista.append(q)
-print((lista))'''
-
-'''lista = sorted([p for p in input().split(',')])
 print(lista)
-print(','.join(lista))'''
 
+#
+lista = sorted([p for p in input().split(',')])
+print(lista)
+print(','.join(lista))
+
+#
 lista = [palavra for palavra in input(': :> ').split(' ')]
-#print(lista)
+
 
 def analisar(lista):
     verificar = set()
@@ -33,3 +37,4 @@ def analisar(lista):
 v = analisar(lista)
 for e in v:
     print(e,end=' ')
+#
