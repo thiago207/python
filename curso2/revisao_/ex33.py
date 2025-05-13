7e71e12e083cd22c6775de7c2c034025816c6428
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-05-12 21:48:34 -0300

    Update ex33.py

--- curso2/revisao_/ex33.py
+++ curso2/revisao_/ex33.py
@@ -1,4 +1,4 @@
-from itertools import groupby
+'''from itertools import groupby
 
 alunos = [
     {'nome': 'Luiz', 'nota': 'A'},
@@ -23,4 +23,25 @@ grupos = groupby(alunos_agrupados, key=ordena)
 for chave, grupo in grupos:
     print(chave)
     for aluno in grupo:
+        print(aluno)'''
+from itertools import groupby
+alunos = [
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Letícia', 'nota': 'B'},
+    {'nome': 'Fabrício', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'},
+]
+def ordena(aluno):
+    return aluno['nota']
+
+alunos_agrupados = sorted(alunos,key=ordena)
+grupos = groupby(alunos_agrupados, key=ordena)
+for grupo,alunos in grupos:
+    print(grupo)
+    for aluno in alunos:
         print(aluno)
