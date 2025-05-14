e6f36f6eb9d915c21cf62a716ae89322186d5112
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-05-13 21:08:17 -0300

    Create ex35.py

---
+++ curso2/revisao_/ex35.py
@@ -0,0 +1,13 @@
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10},
+    {'nome': 'Produto 1', 'preco': 22},
+    {'nome': 'Produto 3', 'preco': 2},
+    {'nome': 'Produto 2', 'preco': 6},
+    {'nome': 'Produto 4', 'preco': 4},
+]
+
+total = sum( p['preco'] for p in produtos
+        if p['nome'] in 'Produto 5'  'Produto 4')
+            
+print(total) 
