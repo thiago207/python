4d32d65763327321dbd8e3fbb22779fb6db96216
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-05-13 21:16:35 -0300

    Update ex35.py

--- curso2/revisao_/ex35.py
+++ curso2/revisao_/ex35.py
@@ -6,8 +6,29 @@ produtos = [
     {'nome': 'Produto 2', 'preco': 6},
     {'nome': 'Produto 4', 'preco': 4},
 ]
+print(*produtos, sep='\n')
+soma_geral = sum(p['preco'] for p in produtos)
+print('soma geral',soma_geral)
 
+print()
+produtos_acima_de_7 = [
+    {**p} for p in produtos
+    if p['preco'] > 7
+]
+soma = sum( p['preco'] for p in produtos
+           if p['preco'] > 7
+           )
+
+print(*produtos_acima_de_7, sep='\n')
+print('soma valores acima de 7:',soma)
+
+lista_filtrada = [
+    {**p} for p in produtos
+    if p['nome'] in 'Produto 5'  'Produto 4'
+
+]
+print(*lista_filtrada, sep='\n')
 total = sum( p['preco'] for p in produtos
         if p['nome'] in 'Produto 5'  'Produto 4')
             
-print(total) 
+print('soma dos produtos 4 e 5:',total) 
