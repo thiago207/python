f737bb223a31679733fd523447e3ee79e51f5ac7
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-05-19 23:04:48 -0300

    Update ex40.py

--- curso2/revisao_/ex40.py
+++ curso2/revisao_/ex40.py
@@ -8,8 +8,33 @@
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
 
+def listar(tarefas):
+    if tarefas:
+        print('\nTarefas:')
+        for e,v in enumerate(tarefas):
+                print(f'{e+1} - {v}')
+    else:
+        print('Lista vazia')
+
+def desfazer(tarefas, tarefas_refazer):
+    if tarefas:
+            tarefa = tarefas.pop()
+            print(f'{tarefa=} removida da lista de tarefas.')
+            tarefas_refazer.append(tarefa)        
+    else:
+            print('Lista vazia')
+
+def refazer(tarefas, tarefas_refazer):
+    if tarefas_refazer:
+            tarefa = tarefas_refazer.pop()
+            print(f'{tarefa=} adicionada a lista de tarefas.')
+            tarefas.append(tarefa)
+    else:
+        print('Nada para refazer')
+
 lista_main = []
-desfeitos = []
+tarefas_refazer = []
+
 while True:
     print('COMANDOS : listar / refazer / desfazer')
 
@@ -17,27 +42,19 @@ while True:
     comando = opc.upper().strip()
     
     if comando == 'LISTAR':
-        print('\nTarefas:')
-        if lista_main:
-            for e,v in enumerate(lista_main):
-                print(f'{e+1} - {v}')
-        else:
-            print('Lista vazia')
+        listar(lista_main)
+
     elif comando == 'DESFAZER':
-        if lista_main:
-            desfeitos = lista_main.pop()
-            print('COMANDO FEITO')
-        else:
-            print('erro')
-    elif comando == 'REFAZER':
-        if desfeitos:
-            lista_main.append(desfeitos)
-        else:
-            print('erro')
-    else:
-        lista_main.append(opc)
+        desfazer(lista_main, tarefas_refazer)
         
-    if comando not in 'LISTAR':
-        print()
-        print(*lista_main, sep='\n')
-        print()
+            
+    elif comando == 'REFAZER':
+        refazer(lista_main, tarefas_refazer)
+
+    else:
+         lista_main.append(opc) 
+ 
+
+    print()
+    print(*lista_main, sep='\n')
+    print()
