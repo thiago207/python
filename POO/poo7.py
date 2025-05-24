7b02f43996c5e23152e2b27b618f2d52f8eb8b4d
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-05-24 14:53:22 -0300

    __dict__ e vars para atributos de instância

--- POO/poo7.py
+++ POO/poo7.py
@@ -1,18 +1,25 @@
-from datetime import date
-
+# __dict__ e vars para atributos de instância
 class Pessoa:
-    ano_atual = date.today().year
+    ano_atual = 2022
 
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
-    
-    def ano_nascimento(self):
-        if isinstance(self.idade, str):
-            self.idade = int(self.idade)
 
-        return f'{self.nome} < nasceu em > {Pessoa.ano_atual - self.idade}'
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
 
-p1 = Pessoa('thiago', '18')
-print(Pessoa.ano_atual)
-print(p1.ano_nascimento())
+
+dados = {'nome': 'João', 'idade': 35}
+p1 = Pessoa(**dados)
+# p1.nome = 'EITA'
+# print(p1.idade)
+# p1.__dict__['outra'] = 'coisa'
+# p1.__dict__['nome'] = 'EITA'
+# del p1.__dict__['nome']
+# print(p1.__dict__)
+# print(vars(p1))
+# print(p1.outra)
+# print(p1.nome)
+print(vars(p1))
+print(p1.nome)
