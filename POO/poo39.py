c368a60ff74ab933551f0b7b62a8fc7b629b8d09
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-06-11 23:45:41 -0300

    Create poo39.py

---
+++ POO/poo39.py
@@ -0,0 +1,13 @@
+class Time:
+    def __init__(self, nome):
+        self.nome = nome
+
+class Planeta:
+    def __init__(self, nome):
+        self.nome = nome
+
+    
+brasil = Time('Brasil')
+portugal = Time('Portugual')
+terra = Planeta('Terra')
+marte = Planeta('Marte')
